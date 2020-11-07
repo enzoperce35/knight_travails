bc24db530e90da3b65393afabdb981cab3469027
enzoperce35 <enzoperce35@gmail.com>
2020-11-07 08:35:19 +0800

    modified

--- knight.rb
+++ knight.rb
@@ -1,16 +1,17 @@
 require_relative 'node.rb'
 
 class Knight < Node
-  attr_accessor :path, :path_queues, :finish
+  attr_accessor :path, :finish, :paths, :visited
 
   def initialize(start, finish)
     @path = Node.new(start)
-    @path_queues = [start]
     @finish = finish
+    @paths = [start]
+    @visited = []
   end
 
-  def possible_moves(coordinate)
-    pos_moves =
+  def possible_moves(coordinate, used_moves)
+    possible_moves =
       [coordinate[0] + 2, coordinate[1] - 1],
       [coordinate[0] + 2, coordinate[1] + 1],
       [coordinate[0] - 2, coordinate[1] - 1],
@@ -19,19 +20,24 @@ class Knight < Node
       [coordinate[0] + 1, coordinate[1] + 2],
       [coordinate[0] - 1, coordinate[1] - 2],
       [coordinate[0] - 1, coordinate[1] + 2]
-    pos_moves.each { |move| path_queues << move if move[0].between?(1,8) && move[1].between?(1,8) }
+    filter(possible_moves, used_moves)
   end
 
-  def travail(visited = [], current = path_queues.shift)
-    visited << current
-    possible_moves(current)
-    path_queues.each do |move|
+  def filter(set1, set2, arr = [])
+    set1.each do |i|
+      arr << i if i[0].between?(1,8) && i[1].between?(1,8)
+    end
+    arr - set2
+  end
+
+  def travail(current = paths.shift)
+    paths.push(*possible_moves(current, visited << current))
+    paths.each do |move|
       temp = path
       temp = temp.next_move until temp.next_move.nil?
       temp.next_move = Node.new(move, current)
       return path if move == finish
     end
-    @path_queues -= visited
-    travail(visited)
+    travail
   end
 end
